b39db5f45763dc1ff558bf44bbd3b82c70f6ddd3
jertempest <tempestj@sheridancollege.ca>
2025-01-14 09:04:17 -0500

    added assignment_2 to the repo
    First commit for assignment 2

---
+++ Assignment_2/README.md
@@ -0,0 +1,1 @@
+This is just a simple hello world program with user input.
---
+++ Assignment_2/hello.py
@@ -0,0 +1,4 @@
+# get the user's name
+user_input = input('What is your name? ')
+# print the user's name to the terminal
+print(f"Hello, {user_input}.")
